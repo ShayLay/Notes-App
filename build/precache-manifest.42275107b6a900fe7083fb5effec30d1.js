4436c13aee3f39bf3f77c78fe9f810e7f10d108d
Seye <seye_ogunkoya@yahoo.co.uk>
2020-09-05 22:49:04 +0100

    production build added

---
+++ build/precache-manifest.42275107b6a900fe7083fb5effec30d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155bb7dcba83f2483bee13cc7ff46c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd5388baff89d223f28",
+    "url": "/static/css/main.511cce21.chunk.css"
+  },
+  {
+    "revision": "d54a7cb2592aa9f63165",
+    "url": "/static/js/2.f60ea13e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f60ea13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd5388baff89d223f28",
+    "url": "/static/js/main.8982b4a7.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
---
+++ build/precache-manifest.5e018ad25590e45a4cbdc6cdfa78fd32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7b867636c8f426bb9eb02b616c10cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3accf73da2301eb2842e",
+    "url": "/static/css/main.511cce21.chunk.css"
+  },
+  {
+    "revision": "d54a7cb2592aa9f63165",
+    "url": "/static/js/2.f60ea13e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f60ea13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3accf73da2301eb2842e",
+    "url": "/static/js/main.eef1c137.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
